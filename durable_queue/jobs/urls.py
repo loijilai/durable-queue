6737b7dcde6976c8508b30ae9cc15eb67e849696
loijilai <loijilai@gmail.com>
2026-06-20 17:49:30 +0800

    test: add endpoint testcases

--- durable_queue/jobs/urls.py
+++ durable_queue/jobs/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", JobCreateView.as_view(), name="job-create"),
-    path("<int:pk>/", JobRetrieveView.as_view(), name="job-create"),
+    path("<int:pk>/", JobRetrieveView.as_view(), name="job-detail"),
 ]
