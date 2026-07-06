2d9592dfa6b0307872f4d0e279f2bf1caaa02a98
loijilai <loijilai@gmail.com>
2026-07-06 17:38:15 +0800

    feat: add social identity table

--- durable_queue/jobs/models.py
+++ durable_queue/jobs/models.py
@@ -27,3 +27,31 @@ class TranscriptionJob(models.Model):
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
+
+
+class SocialIdentity(models.Model):
+    GOOGLE = "google"
+    PROVIDER_CHOICES = {
+        GOOGLE: "Google",
+    }
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="social_identities",
+    )
+    provider = models.CharField(
+        max_length=15, choices=PROVIDER_CHOICES
+    )  # 'google' | 'facebook' | ...
+    provider_sub = models.CharField(max_length=255)  # Google 給的那個唯一 id
+    email = models.EmailField()
+    email_verified = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["provider", "provider_sub"],
+                name="unique_socialidentity_provider_sub",
+            )
+        ]
